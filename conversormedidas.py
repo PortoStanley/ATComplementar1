8c907d281552aad31c9b4b878b54809a9f9607f7
@Stanleygmd77 <150810245+PortoStanley@users.noreply.github.com>
2026-03-19 10:30:54 -0300

    Create conversormedidas.py

---
+++ conversormedidas.py
@@ -0,0 +1,7 @@
+print("Insira uma distância em metros")
+
+distancia_metros = float(input("Distância em metros:"))
+distancia_centimentros = distancia_metros * 100
+distancia_milimetros = distancia_metros * 1000
+
+print(f"A distância de {distancia_metros} metros é igual a {distancia_centimentros} centímetros e {distancia_milimetros} milímetros")
