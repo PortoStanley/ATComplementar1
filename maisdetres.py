1790acc369c6f1188867ac1cd7ece7d79569869f
@Stanleygmd77 <150810245+PortoStanley@users.noreply.github.com>
2026-03-19 10:32:34 -0300

    Create maisdetres.py

---
+++ maisdetres.py
@@ -0,0 +1,10 @@
+print("Informe três numeros: ")
+
+numero1 = int(input())
+numero2 = int(input())
+numero3 = int(input())
+
+maximo = max(numero1, numero2, numero3)
+minimo = min(numero1, numero2, numero3)
+
+print("O maior numero é " + str(maximo) + " e o menor é " + str(minimo))
